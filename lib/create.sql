f6fe2499e35b92c92af20a7e2e72eb48c04fb8f8
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-01 18:08:39 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,3 +6,15 @@ create TABLE projects (
   start_date TEXT,
   end_date TEXT
 );
+
+create TABLE user(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+);
+
+create TABLE pledge (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  project_id INTEGER,
+)
