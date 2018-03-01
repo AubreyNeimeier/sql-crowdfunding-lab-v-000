c9be4a8dbff382cc5980e1d06331b2352843de84
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-01 18:40:35 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,10 +11,9 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   #join on projects.id = pledges.project_id
   #join on pledges.user_id = users.id
   #order by user.name
-"SELECT projects.title, pledges.amount, users.name FROM projects
+"SELECT projects.title, pledges.amount FROM projects
  JOIN pledges ON projects.id = pledges.project_id
- INNER JOIN users ON pledges.user_id = users.id
- ORDER BY users.name;"
+ ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
