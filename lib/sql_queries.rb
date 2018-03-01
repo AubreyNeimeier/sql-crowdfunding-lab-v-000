073f5150a678f532e9d7d31143e9e2afd14e3fd7
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-01 19:49:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age, pledge.amount FROM users
- INNER JOIN ON users.id = pledges.user_id
+ INNER JOIN pledges ON users.id = pledges.user_id
  ORDER BY(users.name); "
 end
 
