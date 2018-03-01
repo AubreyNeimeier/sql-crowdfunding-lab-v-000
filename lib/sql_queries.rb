5ce088da195d152c8a5c4a7027bf0bf04b0a4399
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-01 19:36:58 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   #join on projects.id = pledges.project_id
   #join on pledges.user_id = users.id
   #order by user.name
-"SELECT projects.title SUM(pledges.amount) FROM projects
+"SELECT projects.title, SUM(pledges.amount) FROM projects
  JOIN pledges ON projects.id = pledges.project_id
  GROUP BY SUM pledges.amount
  ORDER BY projects.title;"
