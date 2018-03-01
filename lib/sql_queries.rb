1c982d0c3c5b33a3c4ac751ecf7ba5842f28631b
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-01 19:56:05 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS overage FROM users
  INNER JOIN pledges ON users.id = pledges.user_id
  JOIN pledges projects.id = pledges.project_id
- GROUP BY(users.name); "
+ GROUP BY(users.name) HAVING overage > 0; "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
